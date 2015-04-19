5b562520a878c372c8b9f60b920334710672a17b
dognjen <dognjen@yandex.com>
2015-04-19 23:19:45 +0200

    Added comments and deleted unused code.

--- ChaosGameRepresentation.py
+++ ChaosGameRepresentation.py
@@ -64,27 +64,29 @@ if __name__ == "__main__":
     # remove unknown nucleotides
     sequence = sequence.replace("N", "")
 
+	# Create all possible k combinations of base nucleotides
     combinations = list(cartesian([bases] * k))
+	
+	# Calculate the probability (k-mer) of all combinations in a given sequence
     ver = k_mer_probability(sequence, combinations)
 
     print(ver,"\n")
     print(fasta,"\n")
 
+	# create a matrix in which to represent the combinations
     mat = numpy.array([[0.0 for i in range(s)] for j in range(s)], float)
 
-    for i in range(len(combinations)):
-        print(i,i//s,i%s,ver[combinations[i]]*10)
-        mat[i//s, i%s] = ver[combinations[i]]*10
-
+	# fill the matrix with probabilities, as you can tell by the function name
     fill_matrix_with_probabilities(bases, k, [int(i) for i in range(s)], [int(i) for i in range(s)], '')
 
+	# visualization, 
     pylab.imshow(mat, extent=[0, s, 0, s], interpolation='nearest', cmap=cm.gray)
-    novix = [a + 0.4 for a in range(s)]
-    noviy = [a + 0.4 for a in range(s)]
-    imena_x = [str(i) for i in range(s)]
-    imena_y = [str(i) for i in range(s-1, -1, -1)]
-    pylab.xticks(novix, imena_x)
-    pylab.yticks(noviy, imena_y)
+    x_os = [a + 0.4 for a in range(s)]
+    y_os = [a + 0.4 for a in range(s)]
+    label_x = [str(i) for i in range(s)]
+    label_y = [str(i) for i in range(s-1, -1, -1)]
+    pylab.xticks(x_os, label_x)
+    pylab.yticks(y_os, label_y)
     pylab.show()
 
 
